afd2ba4704e476e89b30648547b31af1ed909816
Shugabuga <bp1004blogs@gmail.com>
2016-01-17 16:21:23 -0500

    Fixes

--- porter.sh
+++ porter.sh
@@ -1,9 +1,11 @@
 echo Porter Installer initiatied.
-echo [NOTICE] Please make sure git, apt-get, amd wget are installed.
+echo [NOTICE] Please make sure git, apt-get, and wget are installed. 
 echo [NOTICE] Please make sure that the Raspberry Pi Official Touchscreen is connected.
 sleep 2
 echo Refreshing sources...
 sudo apt-get update
+echo Installing required dependencies for Porter...
+sudo apt-get install -y --force-yes matchbox x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
 echo Installing required libraries for Chromium...
 sudo apt-get install -y --force-yes libnss3 libxrender1 libxss1 libgtk2.0-0 libgconf2-4
 echo Installing Chromium
@@ -20,7 +22,7 @@ echo Installing Porter
 cd /
 sudo git clone https://github.com/lanewinfield/porter.git
 echo Installing Apache...
-sudo apt-get install apache2
+sudo apt-get install -y --force-yes apache2
 echo Configuring Porter...
 sudo mv /porter/config_files/config.txt /boot/config.txt
 sudo mv /porter/config_files/default /etc/apache2/sites-available/default
@@ -33,4 +35,25 @@ echo Loading configuration file for Porter...
 echo [NOTICE] A reboot is needed. Please reboot after configuring Porter.
 sleep 2
 sudo nano /home/pi/screen/js/config.js
-echo [NOTICE] Please run $ sudo reboot
+echo [NOTICE] Rebooting Raspberry Pi in 10 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 9 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 8 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 7 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 6 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 5 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 4 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 3 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 2 seconds. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi in 1 second. Click CONTROL + C to cancel.
+sleep 1
+echo [NOTICE] Rebooting Raspberry Pi...
+sudo reboot
